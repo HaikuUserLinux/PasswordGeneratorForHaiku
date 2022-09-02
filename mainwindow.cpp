4ec036e4d93974ac918f5919fa041a388bf1bdea
HaikuUserLinux <112551561+HaikuUserLinux@users.noreply.github.com>
2022-09-02 20:23:18 +1000

    Add files via upload

---
+++ mainwindow.cpp
@@ -0,0 +1,40 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+#include <stdlib.h>
+#include <iostream>
+using namespace std;
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+    this->setFixedSize(this->size().width(), this->size().height());
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+
+void MainWindow::on_pushButton_clicked()
+{
+    string symbols = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+    int len = ui->textEdit->toPlainText().toInt();
+    QString res = "";
+    int ind;
+    for (int i = 0; i < len; i++)
+    {
+        ind = 0 + rand() % (symbols.length() - 0);
+        res += symbols[ind];
+    }
+    ui->textEdit_2->setText(res);
+}
+
+
+void MainWindow::on_pushButton_2_clicked()
+{
+    ui->textEdit->setText("");
+    ui->textEdit_2->setText("");
+}
+
